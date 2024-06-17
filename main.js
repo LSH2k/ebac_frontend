e7b7bb659570cfb0d98e6eef59c50275c092017a
Leandro Sousa <156814218+LSH2k@users.noreply.github.com>
2024-06-17 09:56:16 -0300

    some changes

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 const form = document.querySelector('form')
 
-const numA = document.querySelector('input#numA'.value)
-const numB = document.querySelector('input#numB'.value)
+const numA = document.querySelector('input#numA')
+const numB = document.querySelector('input#numB')
 
 function validaNum(N1, N2) {
-    N1 = numA
-    N2 = numB
     return N2 > N1
 }
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
+
+    numValido = validaNum(numA.value, numB.value)
+
+    const invalido = document.querySelector('p#invalido')
+    const certo = document.querySelector('p#certo')
+    
+    if(!numValido) {
+        invalido.innerHTML = 'O Número A deve ser menor que o Número B!'
+        invalido.style.display = 'block'
+        certo.style.display = 'none'
+        document.querySelector('input#btn-enviar').disabled = true
+    }else{
+        certo.innerHTML = `Número A: <strong>${numA}</strong> é menor que o Número B: <strong>${num}</strong>!`
+        certo.style.dispplay = 'block'
+        invalido.style.display = 'none'
+    }
 })
-
-numValido = validaNum(numA, numB)
-
-if (!validaNum(numValido)) {
-    document.querySelector('input#btn-enviar').disabled = true
-}
