dcacf7d55c426738c4af9a8eceb36a0b661ada75
Leandro Sousa <156814218+LSH2k@users.noreply.github.com>
2024-08-04 14:11:29 -0300

    Added scripts

---
+++ build/scripts/sum.js
@@ -0,0 +1,1 @@
+function ಠ_ಠ387(o,r){return o+r}module.ಠ_ಠ388=ಠ_ಠ387;
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
 
-function funcaoPadrao(callback) {
-    setTimeout(function() {
-        console.log('Executando via gulp')
-        callback()
-    }, 2000)
+function comprimeImg() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
 }
 
-function dizOi(callback) {
-    setTimeout(function() {
-        console.log('Olá, Gulp!')
-        dizTchau()
-        callback()
-    },1000)
-    
+function comprimeJs() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
 }
 
-function dizTchau() {
-    console.log('Tchau, Gulp!')
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
 }
 
-exports.default = gulp.paralell(funcaoPadrao, dizOi)
-exports.dizOi = dizOi
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false} , gulp.series(compilaSass))
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false} , gulp.series(comprimeJs))
+    gulp.watch('./source/images/*', {ignoreInitial: false} , gulp.series(comprimeImg))
+}
